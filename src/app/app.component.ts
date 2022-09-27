536e8e16353d78d1035b7aaaf60df3f2501a276b
marvintik <64403964+marvintik@users.noreply.github.com>
2022-09-27 18:32:27 +0300

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {SignatureComponent} from "./signature/signature.component";
+import {MatDialog} from "@angular/material/dialog";
+import jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agreementPdfDemo';
+  signature: any;
+
+  firstName: string;
+  lastName: string;
+  isSing = false;
+  signatureDate;
+  @ViewChild('agreementDiv') content:ElementRef;
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  sign() {
+    let dialogRef = this.dialog.open(SignatureComponent, this.getCustomDialogConfig(null, '800px', '520px'));
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res.data !== undefined) {
+        this.signature = res.data;
+        this.isSing = true;
+        this.signatureDate = new Date();
+      }
+    });
+  }
+
+  private getCustomDialogConfig(data: any, width: string, height: string) {
+    return {
+      width: width,
+      height: height,
+      panelClass: 'customDialog',
+      data: data,
+    };
+  }
+
+  savePdf() {
+    let content = this.content.nativeElement;
+
+    var nativeElement = document.getElementById('contentAgr');
+    const doc = new jsPDF({
+      orientation: 'p',
+      unit: 'px',
+      format: 'a4',
+      putOnlyUsedFonts:true,
+      hotfixes: ["px_scaling"],
+    //  floatPrecision: "smart" // or "smart", default is 16
+    });
+
+
+    doc.html(nativeElement, {
+
+      autoPaging: 'text',
+      windowWidth: 750,
+      width: 750,
+      margin: [10, 10, 10, 20]
+    }).then(() => doc.save("a4.pdf")
+  );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgreementComponent } from './agreement/agreement.component';
+import { SignatureComponent } from './signature/signature.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
+import {SignaturePadModule} from "angular2-signaturepad";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgreementComponent,
+    SignatureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    SignaturePadModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
